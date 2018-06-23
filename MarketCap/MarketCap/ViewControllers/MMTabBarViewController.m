f4696b46baaed709fbd335e03dfc6de7f4095d42
Daniel Stewart <daniel@paradimeapp.com>
2018-06-23 11:41:08 -0400

    Create clear color selection cell

--- MarketCap/MarketCap/ViewControllers/MMTabBarViewController.m
+++ MarketCap/MarketCap/ViewControllers/MMTabBarViewController.m
@@ -11,6 +11,7 @@
     [super viewDidLoad];
     
     self.tabBar.backgroundColor = [UIColor colorWithRed: 29.0 / 255.0 green: 29.0 / 255.0  blue: 29.0 / 255.0  alpha: 1.0];
+    
     self.tabBar.barTintColor = [UIColor colorWithRed: 29.0 / 255.0 green: 29.0 / 255.0  blue: 29.0 / 255.0  alpha: 1.0];
     [[UITabBarItem appearance] setTitleTextAttributes: @{ NSForegroundColorAttributeName : [UIColor whiteColor] }
                                              forState: UIControlStateNormal];
