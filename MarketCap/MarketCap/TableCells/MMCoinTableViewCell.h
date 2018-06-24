be6ff4cd803a05bad582e59f0ac267fa4866247f
Daniel Stewart <daniel@paradimeapp.com>
2018-06-23 23:18:37 -0400

    TableCellUpdates

--- MarketCap/MarketCap/TableCells/MMCoinTableViewCell.h
+++ MarketCap/MarketCap/TableCells/MMCoinTableViewCell.h
@@ -3,6 +3,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *coinRank;
 @property (weak, nonatomic) IBOutlet UIImageView *coinIcon;
 @property (weak, nonatomic) IBOutlet UILabel *coinName;
+@property (weak, nonatomic) IBOutlet UILabel *coinSymbol;
+
 @property (weak, nonatomic) IBOutlet UILabel *coinPrice;
 @property (weak, nonatomic) IBOutlet UILabel *percentageChange;
 @property (weak, nonatomic) IBOutlet MMFavoritesButton *favoritesButton;
